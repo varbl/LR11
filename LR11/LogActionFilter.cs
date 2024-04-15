60e8491f354f563bea80dc3091a5a18f8acc8822
Valerii Bardakov <Valerii Bardakov@ASUS_M509DJ>
2024-04-15 23:42:49 +0300

    Добавьте файлы проекта.

---
+++ LR11/LogActionFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace LR11
+
+{
+    public class LogActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+            string actionName = filterContext.ActionDescriptor.ActionName;
+            string logMessage = $"Action '{actionName}' in controller '{controllerName}' called at {DateTime.Now}";
+
+            WriteToLogFile(logMessage);
+        }
+
+        private void WriteToLogFile(string message)
+        {
+            string filePath = @"C:\Logs\actionLog.txt"; 
+
+            using (StreamWriter writer = File.AppendText(filePath))
+            {
+                writer.WriteLine(message);
+            }
+        }
+    }
+}
